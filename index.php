98cc955be503e77607033c5e2ee6e71d20416854
michelle.bruemmer <michelle.bruemmer@gmail.com>
2011-11-25 11:17:12 +0000

    Prototyp hochgeladen

--- index.php
+++ index.php
@@ -1,11 +1,45 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Project 'Phyton'</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Projekt Hydra</title>
+<link href="resource/stylesheet/style.css" rel="stylesheet" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Mako' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Bevan' rel='stylesheet' type='text/css'>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
+<script type="text/javascript">
+
+</script>
+
 </head>
+
 <body>
-    <p>
-        Initial html file please add some more code to the project
-    </p>
+	<div id="header">
+    <img id="logo" src="resource/images/logo.jpg" alt="logo" />
+    </div>
+	<div id="content">
+		<h1>Welcome. Log in.</h1>
+		<form  action="" method="post" accept-charset="ISO-8859-1">
+			<input class="formfield" name="email" type="text" size="40" maxlength="40" value="Enter your email adress.">
+ <input class="button" type="button" name="go" value="Go"
+      onclick="">
+            <p><a href="#">Register an account.</a></p>
+	</form>
+	</div>
+    
+    <div id="footer">
+    	<ul id="footer_nav">
+        	<li>
+            <a href="#">Impressum<a>
+            </li>
+         	<li>
+            <a href="#">Kontakt<a>
+            </li>           
+            <li>
+           <a href="#"> Hilfe<a>
+            </li>           
+    	</ul>
+    </div>
 </body>
 </html>
