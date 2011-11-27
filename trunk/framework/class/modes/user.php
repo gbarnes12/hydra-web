3927ac3558e8f83b612bab4ca6d144d656c4f3b4
gavin.barnes90@gmail.com <gavin.barnes90@gmail.com@8d898c67-a961-0069-eae3-6bf9736cf65a>
2011-11-27 12:06:04 +0000

    - added the checkIfUserExists method to the backend and frontend thus we can check if the email address is already taken. - changed some names due to spell errors. - added the functionality to clear content of an input field when one clicks into the field and fill it again if one leaves it!

--- trunk/framework/class/modes/user.php
+++ trunk/framework/class/modes/user.php
@@ -11,5 +11,41 @@
      */
     class user
     {
+        /**
+         * user::checkIfUserExists()
+         * 
+         * Is used to check if a given user
+         * exists in the database.
+         * 
+         * @return array Either of type error or notify
+         */
+        public function checkIfUserExists()
+        {
+            $mysql = framework::$mysql;
+            
+            if($_POST["email"])
+            {
+                $sql = "SELECT email FROM awe_users WHERE email='".$mysql->SecureVariable($_POST["email"])."'";
+                $return = $mysql->Command($sql, true);
+                
+                if(count($return) > 0) 
+                {
+                    return array("type" => "return", "value" => "true");
+                }
+                
+                return array("type" => "return", "value" => "false");
+            }
+            
+            return array("type" => "error", "message" => "You haven't supplied any email address to check");
+        }
         
+        public function getUser()
+        {
+            
+        }
+        
+        public function registerUser()
+        {
+            
+        }
     }
